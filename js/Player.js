960afcd7155c374d2258bbeaed65c93a0b2cf514
Mitch Tee <mitch.tee@gmail.com>
2014-01-25 02:23:22 -0800

    Flip sprite direction.

--- js/Player.js
+++ js/Player.js
@@ -2,12 +2,16 @@
   Player 1, Swordfish cat
 */
 
+var LEFT = 0;
+var RIGHT = 1;
+
 // I guess this is like a constructor
 Player = function(game) {
   this.game = game;
   this.sprite = null;
   // Cursors are how we control the player
   this.cursors = null;
+  this.direction = RIGHT;
 };
 
 Player.prototype = {
@@ -54,14 +58,18 @@ Player.prototype = {
     var isAirborne = !this.sprite.body.touching.down;
     var jumpSpeed = -800;
     var runSpeed = isAirborne ? 180: 250;
+
     this.sprite.body.velocity.x = 0;
+    this.sprite.anchor.x = 0.5;
 
     // Check movement
     if (this.leftKey.isDown) {
       this.sprite.body.velocity.x = -runSpeed;
+      this.tryFaceCorrectDirection(LEFT);
     }
     else if (this.rightKey.isDown) {
       this.sprite.body.velocity.x = runSpeed;
+      this.tryFaceCorrectDirection(RIGHT);
     }
     else {
       // stop animation code will go here
@@ -76,8 +84,16 @@ Player.prototype = {
   },
 
   addPhysics: function() {
-    this.sprite.body.bounce.y = 0.2;
+    this.sprite.body.bounce.y = 0.1;
     this.sprite.body.gravity.y = 30;
     this.sprite.body.collideWorldBounds = true;
+  },
+
+  tryFaceCorrectDirection: function(tryTurnDirection) {
+    if (tryTurnDirection == this.direction) {
+      return;
+    }
+    this.direction = tryTurnDirection;
+    this.sprite.scale.x *= -1;
   }
 }
