e47e21111ca172cf60913e767ae00c218d735535
Olivia Zhang <olivia_ny_zhang@hotmail.com>
2014-01-26 11:51:18 -0800

    added level transition + bug fix

--- js/Game.js
+++ js/Game.js
@@ -1,12 +1,12 @@
 // Level is global
 // All managers are global
-GameObjectManager = function(game) {
+Game1ObjectManager = function(game) {
 	this.game = game;
 	level = null;
   tableManager = null;
 }
 
-GameObjectManager.prototype = {
+Game1ObjectManager.prototype = {
 
 	preload: function() {
     this.load.spritesheet('dog', 'assets/sprites/dogsheet.png', 64, 80);
@@ -49,6 +49,128 @@ GameObjectManager.prototype = {
     	player1.update();
     	player2.update();
     	tableManager.update(); 
+      level.update();
+    },
+    render: function() {
+      this.game.debug.renderSpriteBody(level.floor2);
+    }
+}
+
+
+
+//second level
+
+// Level is global
+// All managers are global
+Game2ObjectManager = function(game) {
+  this.game = game;
+  level = null;
+  tableManager = null;
+}
+
+Game2ObjectManager.prototype = {
+
+  preload: function() {
+    this.load.spritesheet('dog', 'assets/sprites/dogsheet.png', 64, 80);
+    this.load.spritesheet('cat', 'assets/sprites/catsheet.png', 64, 64);
+    this.load.spritesheet('world', "assets/sprites/catdogworldsheet.png", 80, 80);
+    this.load.spritesheet('catworld', "assets/sprites/catworldsheet.png", 80, 80);
+    this.load.spritesheet('dogworld', "assets/sprites/dogworldsheet.png", 80, 80);
+    this.load.image('dogwin', "assets/backgrounds/dogwin.png");
+    this.load.image('fliplevel', "assets/backgrounds/fliptonext.png");
+    this.load.image('catwin', "assets/backgrounds/catwin.png");
+    this.load.spritesheet('table', 'assets/sprites/tablesheet.png', 80, 32);
+    _this = this;
+    level = new Level2(_this);
+    player1 = new Player(_this);
+    player2 = new Player2(_this);
+    tableManager = new Table(_this);
+    globe = new Globe(_this);
+    hud = new Hud(_this);
+
+      level.preload();
+      globe.preload();
+      player1.preload();
+      player2.preload();
+      tableManager.preload();
+      hud.preload();
+  },
+
+  create: function () {
+      level.create();
+      globe.create();
+      player1.create();
+      player2.create();
+      tableManager.create();
+      hud.create();
+    },
+
+    update: function () {
+
+      globe.update();
+      player1.update();
+      player2.update();
+      tableManager.update(); 
+      level.update();
+    },
+    render: function() {
+      this.game.debug.renderSpriteBody(level.floor2);
+    }
+}
+
+
+//original level
+// Level is global
+// All managers are global
+Game3ObjectManager = function(game) {
+  this.game = game;
+  level = null;
+  tableManager = null;
+}
+
+Game3ObjectManager.prototype = {
+
+  preload: function() {
+    this.load.spritesheet('dog', 'assets/sprites/dogsheet.png', 64, 80);
+    this.load.spritesheet('cat', 'assets/sprites/catsheet.png', 64, 64);
+    this.load.spritesheet('world', "assets/sprites/catdogworldsheet.png", 80, 80);
+    this.load.spritesheet('catworld', "assets/sprites/catworldsheet.png", 80, 80);
+    this.load.spritesheet('dogworld', "assets/sprites/dogworldsheet.png", 80, 80);
+    this.load.image('dogwin', "assets/backgrounds/dogwin.png");
+    this.load.image('fliplevel', "assets/backgrounds/fliptonext.png");
+    this.load.image('catwin', "assets/backgrounds/catwin.png");
+    this.load.spritesheet('table', 'assets/sprites/tablesheet.png', 80, 32);
+    _this = this;
+    level = new Level(_this);
+    player1 = new Player(_this);
+    player2 = new Player2(_this);
+    tableManager = new Table(_this);
+    globe = new Globe(_this);
+    hud = new Hud(_this);
+
+      level.preload();
+      globe.preload();
+      player1.preload();
+      player2.preload();
+      tableManager.preload();
+      hud.preload();
+  },
+
+  create: function () {
+      level.create();
+      globe.create();
+      player1.create();
+      player2.create();
+      tableManager.create();
+      hud.create();
+    },
+
+    update: function () {
+
+      globe.update();
+      player1.update();
+      player2.update();
+      tableManager.update(); 
       level.update();
     },
     render: function() {
--- js/Preloader.js
+++ js/Preloader.js
@@ -6,6 +6,10 @@ Preloader.prototype = {
     preload: function () {
     	this.game.load.image('splash', 'assets/backgrounds/splashfinal.png');
         this.game.load.image('start', 'assets/sprites/startbutton.png');
+
+        this.game.load.image('level1', 'assets/sprites/level1.png');
+        this.game.load.image('level2', 'assets/sprites/level2.png');
+        this.game.load.image('level3', 'assets/sprites/level3.png');
     },
 
     create: function () {
