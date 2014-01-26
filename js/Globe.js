1121db887116f47ecac0bd36a88e85881d050cc7
Olivia Zhang <olivia_ny_zhang@hotmail.com>
2014-01-26 00:30:49 -0800

    added globe class

---
+++ js/Globe.js
@@ -0,0 +1,20 @@
+Globe = function(game) {
+	this.game = game;
+}
+
+Globe.prototype = {
+
+	preload: function(){
+		this.game.load.sprite('globe1', 'assets/')
+	},
+
+	create: function() {
+
+	},
+
+	update: function() {
+
+	}
+
+
+}
--- js/MainMenu.js
+++ js/MainMenu.js
@@ -5,7 +5,7 @@ MainMenu = function(game) {
 MainMenu.prototype = {
     create: function () {
     	splash_screen = this.game.add.sprite(0, 0, 'splash');
-    	splash_screen.scale.setTo(0.72, 0.72);
+    	splash_screen.scale.setTo(0.72, 0.75);
 
         button = this.game.add.button(this.game.world.centerX, 400, 'start', this.startGame, this, 2, 1, 0);
        	button.scale.setTo(0.7, 0.7);
